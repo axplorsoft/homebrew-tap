25508229ecd98ebfb241314032d47ad8f0f2c19e
Parag Pramod Radke <paragradke@gmail.com>
2025-10-10 19:15:32 +0530

    Update checksum

--- Casks/axplor.rb
+++ Casks/axplor.rb
@@ -6,7 +6,7 @@ cask "axplor" do
     sha256 "fec7589b65d5a930426ad08d1df711f72836c7871d4da1ad7de551e9a022ae70"
   else
     url "https://axplor-builds.s3.ap-south-1.amazonaws.com/platforms/macos/x86/releases/axplor-0.0.26-macos-x86_64.tar.gz"
-    sha256 "4303924e3510124a6458f66a3496c7eb2bd005252e9be49d29a154f082fb7e61"
+    sha256 "4a02fa1948ee3d28a7d034d53ed16c1914bf946cdb57264207e12b351170506b"
   end
 
   name "Axplor"
