c2d1552d3a564cbecfca5bb7037f0cbd6496a8f5
Parag Pramod Radke <paragradke@gmail.com>
2025-10-27 19:53:22 +0530

    Upgrade application to version 0.0.31

--- Casks/axplor.rb
+++ Casks/axplor.rb
@@ -1,12 +1,12 @@
 cask "axplor" do
-  version "0.0.30"
+  version "0.0.31"
 
   if Hardware::CPU.arm?
-    url "https://axplor-builds.s3.ap-south-1.amazonaws.com/platforms/macos/arm/releases/axplor-0.0.30-macos-arm.app.tar.gz"
-    sha256 "f5435acca177e1eeeed2dc803565afeae9643ab2c7a235cc738f1bc9cccb0b4d"
+    url "https://axplor-builds.s3.ap-south-1.amazonaws.com/platforms/macos/arm/releases/axplor-0.0.31-macos-arm.app.tar.gz"
+    sha256 "ac0ff2b6ef312df448ad704853bee679e6e3c584a20c0ad1404487505e8e1f62"
   else
-    url "https://axplor-builds.s3.ap-south-1.amazonaws.com/platforms/macos/x86/releases/axplor-0.0.30-macos-x86.app.tar.gz"
-    sha256 "6dacbfbe258f14e22abc91056c5f8ed566ff778109bbcbad763559d914de7110"
+    url "https://axplor-builds.s3.ap-south-1.amazonaws.com/platforms/macos/x86/releases/axplor-0.0.31-macos-x86.app.tar.gz"
+    sha256 "faecf9e3355dc7b636a8d942a9658ec1991e28400ad80c691454c3ffa2ec3f0b"
   end
 
   name "Axplor"
